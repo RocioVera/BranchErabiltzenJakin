92c97658167c29357c7add7c5d701ddcbaeaf006
mañana <maana@ORD27>
2019-03-07 13:02:31 +0100

    aldaketak

--- liburutegia/src/liburutegia/Liburuak.java
+++ liburutegia/src/liburutegia/Liburuak.java
@@ -1,5 +1,43 @@
 package liburutegia;
 
-public class liburua {
+public class Liburuak {
+	private String izenburua, ISBN;
+	private int orrialdeKopurua;
+	
+	//konstruktorea
+	public Liburuak(String izenburua, String ISBN, int orrialdeKopurua) {
+		this.izenburua = izenburua;
+		this.ISBN = ISBN;
+		this.orrialdeKopurua = orrialdeKopurua;
+	}
+	
+	//get
+	public String getIzenburua() {
+		return izenburua;
+	}
+	public String getISBN() {
+		return ISBN;
+	}
+	public int getOrrialdeKopurua() {
+		return orrialdeKopurua;
+	}
 
+	//set
+	public void setIzenburua(String izenburua) {
+		this.izenburua = izenburua;
+	}
+
+	public void setISBN(String iSBN) {
+		ISBN = iSBN;
+	}
+	public void setOrrialdeKopurua(int orrialdeKopurua) {
+		this.orrialdeKopurua = orrialdeKopurua;
+	}
+
+	//toString
+	@Override
+	public String toString() {
+		return "Liburuak [izenburua=" + izenburua + ", ISBN=" + ISBN + ", orrialdeKopurua=" + orrialdeKopurua + "]";
+	}
+	
 }
--- liburutegia/src/liburutegia/Liburutegia.java
+++ liburutegia/src/liburutegia/Liburutegia.java
@@ -1,5 +1,53 @@
 package liburutegia;
+import java.util.ArrayList;
 
-public class liburutegia {
+public class Liburutegia {
+	private String izena, helbidea;
+	private int ikasKop;
+	private ArrayList <Liburuak> liburua = new ArrayList<Liburuak>(5);
 
+	//konstruktorea
+	public Liburutegia(String izena, String helbidea, int ikasKop, ArrayList<Liburuak> liburua) {
+		this.izena = izena;
+		this.helbidea = helbidea;
+		this.ikasKop = ikasKop;
+		this.liburua = liburua;
+	}
+
+	//get
+	public String getIzena() {
+		return izena;
+	}
+	
+	public String getHelbidea() {
+		return helbidea;
+	}
+	public int getIkasKop() {
+		return ikasKop;
+	}
+	public ArrayList<Liburuak> getLiburua() {
+		return liburua;
+	}
+	
+	//set
+	public void setIzena(String izena) {
+		izena = izena;
+	}
+	public void setHelbidea(String helbidea) {
+		this.helbidea = helbidea;
+	}
+	public void setIkasKop(int ikasKop) {
+		this.ikasKop = ikasKop;
+	}
+	public void setLiburua(ArrayList<Liburuak> liburua) {
+		this.liburua = liburua;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return "Liburutegia [izena=" + izena + ", helbidea=" + helbidea + ", ikasKop=" + ikasKop + ", liburua="
+				+ liburua + "]";
+	}
+	
 }
